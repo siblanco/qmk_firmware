bbeef407b1fa7073cc2df9bbb2e27609fac5df7b
Hassan El Siblani <hallo@siblanco.dev>
2022-09-05 16:59:28 +0200

    numpad on left

--- keyboards/crkbd/keymaps/sytecs_corne/keymap.c
+++ keyboards/crkbd/keymaps/sytecs_corne/keymap.c
@@ -27,19 +27,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            MT(MOD_LSFT, KC_ESC), KC_SPC, LT(1, KC_TAB), MO(2), MT(MOD_LCTL, KC_ENT), MT(MOD_LGUI, KC_BSPC)
+                    MT(MOD_LSFT, KC_ESC), LT(2, KC_SPC), LT(3, KC_TAB), MT(MOD_LCTL, KC_ENT), LT(1, KC_BSPC), KC_LGUI
                                       //`--------------------------'  `--------------------------'
-
   ),
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+      _______, XXXXXXX,   KC_7,     KC_8,    KC_9, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX,   KC_4,    KC_5,     KC_6, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     _______,  XXXXXXX,   KC_1,    KC_2,     KC_3, KC_MINS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,     MO(3), _______, _______
+                                          _______,    KC_0, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
   [2] = LAYOUT_split_3x6_3(
