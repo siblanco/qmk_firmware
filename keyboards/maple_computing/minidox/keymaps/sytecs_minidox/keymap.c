fe57d0c79837bebee80273f12120566263e94c2c
Hassan El Siblani <hallo@siblanco.dev>
2022-10-05 22:43:51 +0200

    change base to colemakdh accordin to miryoku

--- keyboards/maple_computing/minidox/keymaps/sytecs_minidox/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/sytecs_minidox/keymap.c
@@ -20,11 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x5_3(
-      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                 KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
+      KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                 KC_J,    KC_L,    KC_U,    KC_Y,   KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_A, LGUI_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F), KC_G,                       KC_H, MT(MOD_RSFT, KC_J), MT(MOD_RCTL, KC_K), RGUI_T(KC_L), KC_SCLN, 
+      KC_A, LGUI_T(KC_R),LCTL_T(KC_S),LSFT_T(KC_T), KC_G,                       KC_M, MT(MOD_RSFT, KC_N), MT(MOD_RCTL, KC_E), RGUI_T(KC_I), KC_O, 
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
+      KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                                 KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                LT(6, KC_ESC), LT(3, KC_SPC), LT(4, KC_TAB),   LT(2, KC_ENT), LT(1, KC_BSPC), LT(5, KC_DEL)
                                       //`--------------------------'  `--------------------------'
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_QUOT,    KC_4,    KC_5,    KC_6,  KC_EQL,                              XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_LALT,
+      KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL,                              XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_LALT,
   //|--------+--------+--------+--------+--------+--------                     |--------+--------+--------+--------+--------+--------|
       KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
   //|--------+--------+--------+--------+--------+--------+-------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_DQUO,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,                              XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_LALT, 
+      KC_COLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,                              XXXXXXX, KC_RSFT, KC_RCTL, KC_RGUI, KC_LALT, 
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
